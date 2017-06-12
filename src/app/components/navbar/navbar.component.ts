bb5c28e5993b640f18e4135090ac60f2cf6dd388
Pablo Aragón <pablo.aragon22@gmail.com>
2017-06-12 13:53:49 +0200

    nav route and styles

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'navbar',
@@ -10,6 +11,8 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+     $(".button-collapse").sideNav();
   }
 
 }
