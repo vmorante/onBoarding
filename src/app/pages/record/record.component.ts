edac47cc37b7886fa168d165249e9c6befc34449
Pablo Aragón <pablo.aragon22@gmail.com>
2017-06-13 11:06:50 +0200

    record page

--- src/app/pages/record/record.component.ts
+++ src/app/pages/record/record.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-record',
   templateUrl: './record.component.html',
-  styleUrls: ['./record.component.css']
+  styleUrls: ['./record.component.css'],
 })
-export class RecordComponent implements OnInit {
+export class RecordComponent implements OnInit, OnDestroy {
+
+  @ViewChild('recordVideo') videoEl: ElementRef;
+  recording: boolean;
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  ngOnDestroy(): void {
+    var video = this.videoEl.nativeElement;
+  }
+
+  startRecording() {
+
+    navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: { width: 1280, height: 720 }
+    }).then((stream) => {
+
+      this.recording = true;
+
+      var videoTracks = stream.getVideoTracks();
+
+      var video = this.videoEl.nativeElement;
+      video.srcObject = stream;
+      video.onloadedmetadata = function (e) {
+        video.play();
+      };
+    }).catch(function (err) {
+      console.log(err);
+    });
+  }
 }
