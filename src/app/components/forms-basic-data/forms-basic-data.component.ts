1597295742b89d681240b8fbe43d50eeb6376dfe
Veronica <veronica.morante.pindador@gmail.com>
2017-06-13 10:47:41 +0200

    Formulario

--- src/app/components/forms-basic-data/forms-basic-data.component.ts
+++ src/app/components/forms-basic-data/forms-basic-data.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup } from "@angular/forms";
+
 @Component({
   selector: 'app-forms-basic-data',
   templateUrl: './forms-basic-data.component.html',
@@ -11,5 +13,15 @@ export class FormsBasicDataComponent implements OnInit {
 
   ngOnInit() {
   }
+
+   submitBasicData(form: FormGroup): void {
+       var  userForms=form.value;
+            
+       localStorage.setItem("name",JSON.stringify(userForms.name))  
+       localStorage.setItem("last_name",JSON.stringify(userForms.last_name))   
+       localStorage.setItem("telephone",JSON.stringify(userForms.telephone))   
+       localStorage.setItem("email",JSON.stringify(userForms.email))   
+            
+    }
 
 }
